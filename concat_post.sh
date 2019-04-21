c9fbbee1a2238a8f6d75874d6ba135e2cd4ed822
Lawrence Pan <lawrencefeipan@gmail.com>
2019-04-21 17:22:30 -0400

    update post-midterm

--- concat_post.sh
+++ concat_post.sh
@@ -11,7 +11,10 @@ gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=p0.pdf $notes
 notes=$(find ./notes/ | grep 'L2[0-9]*\.pdf' | sort)
 gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=p1.pdf $notes
 
-notes=$(echo p0.pdf p1.pdf)
+notes=$(find ./notes/ | grep 'L3[0-9]*\.pdf' | sort)
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=p2.pdf $notes
+
+notes=$(echo p0.pdf p1.pdf p2.pdf)
 gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=post.pdf $notes
 
-rm p0.pdf p1.pdf
+rm p0.pdf p1.pdf p2.pdf
